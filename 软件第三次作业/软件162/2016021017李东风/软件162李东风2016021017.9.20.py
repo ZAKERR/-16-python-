eefd132bbb124004eeb2c66292a0dc7d66807a3b
01LDF <43255341+01LDF@users.noreply.github.com>
2018-09-21 12:43:39 +0800

    2018.9.20

---
+++ 软件第三次作业/软件162/2016021017李东风/软件162李东风2016021017.9.20.py
@@ -0,0 +1,16 @@
+x=input(u"请输入你的年龄：")
+x=int(x);
+if x<15:
+    print(u"你此时正处于玩的时候")
+elif x>=15 and x<30:
+    print(u"学习使你快乐")
+elif x>30 and x<40:
+    print(u"能够自理")
+elif x>=40 and x<50:
+    print(u"四十不惑")
+elif x>=50 and x<60:
+    print(u"知天命")
+elif x>=60 and x<70:
+    print(u"耳顺")
+elif x>=70 and x<80:
+    print(u"从心而欲")
