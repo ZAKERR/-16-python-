d759a3fa35ca3b1eb9fcded235829818f813b774
Yee3 <43257769+Yee3@users.noreply.github.com>
2018-09-22 18:08:52 +0800

    2018/9/22

---
+++ 软件第三次作业/软件162/2016021022杨燕芳/皮球.py
@@ -0,0 +1,18 @@
+# !/user/bin/python
+# -*-coding:utf-8-*-
+'''
+编写人：杨燕芳
+编写时间：20180920
+功能：皮球所经过的路程以及第10次反弹高度
+'''
+sum = 100
+high = 100
+for i in range(2,11):
+    high = high / 2
+    sum = sum + high * 2
+print('在第10次落地共经过%fm'%sum)
+print('第10次反弹的高度为%fm'%high)
+
+
+
+
