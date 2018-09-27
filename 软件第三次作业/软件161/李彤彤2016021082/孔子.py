0762c7327df623c9dcc13c17d95e7744b66d89a9
LiTtong <43435516+LiTtong@users.noreply.github.com>
2018-09-27 04:14:14 -0500

    9.27

---
+++ 软件第三次作业/软件161/李彤彤2016021082/孔子.py
@@ -0,0 +1,15 @@
+a=int(input('输入孔子年龄:'))
+if a>0 and a<15:
+   print('长身体')
+elif a>=15 and a<30:
+   print('至于学')
+elif a>=30 and a<40:
+   print('而立')
+elif a>=40 and a<50:
+   print('不惑')
+elif a>=50 and a<60:
+   print('知天命')
+elif a>=60 and a<70:
+   print('而顺')
+elif a>=70:
+   print('不逾矩')
