a2f2056cdfd2174fa1248703fa609f1a25e1201f
Hebejiang <43270093+Hebejiang@users.noreply.github.com>
2018-09-27 14:53:11 +0800

    Add files via upload

---
+++ 软件第三次作业/软件162/2016021060宋延杰/孔子.py
@@ -0,0 +1,15 @@
+a=int(input("请输入孔子的年龄:"))
+if a>=70:
+    print("七十而从心所欲，不逾矩")
+elif a>=60:
+    print("六十而耳顺")
+elif a>=50:
+    print("五十而知天命")
+elif a>=40:
+    print("四十而不惑")
+elif a>=30:
+    print("三十而立")
+elif a>=50:
+    print("吾十有五而致于学")
+else:
+    print("玩耍")
