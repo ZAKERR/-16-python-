6a527d9202b233616fbb9222401ba3b6b87dd985
ifyouseem1 <448711064@163.com>
2018-09-27 17:51:19 +0800

    2018/9/27
    a = int(input("输入a："))
    s = str(input("输入符号："))  
    b = int(input("输入c:"))
    if(s=='+'):
        print(a,"+",b,"=",a+b)
    elif (s=='-'):
        print(a,"-",b,"=",a-b)
    elif (s=='*'):
        print(a,"*",b,"=",a*b)
    elif (s=='/'):
        print(a,"/",b,"=",a/b)

---
+++ 软件第四次作业/软件162/2016021166李慧颖/456789.py
@@ -0,0 +1,12 @@
+a = int(input("输入a："))
+s = str(input("输入符号："))  
+b = int(input("输入c:"))
+if(s=='+'):
+    print(a,"+",b,"=",a+b)
+elif (s=='-'):
+    print(a,"-",b,"=",a-b)
+elif (s=='*'):
+    print(a,"*",b,"=",a*b)
+elif (s=='/'):
+    print(a,"/",b,"=",a/b)
+      
