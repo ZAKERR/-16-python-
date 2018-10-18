7036799a2aa9190ab0b530c5de06be321dc2d58f
Renminb <43255520+Renminb@users.noreply.github.com>
2018-10-18 21:29:02 +0800

    奥运五环 2018.10.18

---
+++ 软件第六次作业/软件162/2016021112张瑞芳/奥运五环.py
@@ -0,0 +1,22 @@
+#功能：运用turtle奥运五环
+#编写人：张瑞芳
+#时间2018.10.18
+
+import turtle
+
+ipaomi=turtle
+ipaomi.shape('turtle')#把画笔设置为小乌龟
+ipaomi.pensize(width=3)#设置画笔宽度
+color = ['blue','black','red','yellow','green']
+p_x = [-110,0,110,-55,55]
+p_y = [-25,-25,-25,-75,-75]
+
+for i in range(5):
+    ipaomi.color(color[i])
+    ipaomi.penup()
+    ipaomi.goto(p_x[i],p_y[i])
+    ipaomi.pendown()
+    ipaomi.circle(45)
+
+turtle.down()
+ 
