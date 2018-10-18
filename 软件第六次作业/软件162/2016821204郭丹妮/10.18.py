0732fd08111b50fa1d2cbf768308d252020a606f
dannyjinx <43272038+dannyjinx@users.noreply.github.com>
2018-10-18 22:23:24 +0800

    10.18

---
+++ 软件第六次作业/软件162/2016821204郭丹妮/10.18.py
@@ -0,0 +1,14 @@
+from turtle import *  
+  
+colors=['blue','black','red','yellow','green']  
+for i in range(5):  
+    x = -100+100*i if i<3 else 50*(-1)**i  
+    y = 50 if i<3 else 0  
+    up(),goto(x,y),width(5),down()  
+    color(colors[i])  
+    circle(40)  
+#  
+color('black')  
+up(),goto(-80,-80),down()  
+write("the Olympic Rings",font=("Aril",16,'bold'))  
+done()  
