40d93499980bafde5b432da7423a43a797aea287
LiHongJie666666 <43258502+LiHongJie666666@users.noreply.github.com>
2018-10-18 14:23:19 +0800

    Add files via upload

---
+++ 软件第三次作业/软件161/李洪杰2016021151/孔子.py
@@ -0,0 +1,11 @@
+#编写人：李洪杰 2016021151
+#编写题目：孔子
+x=input("输入你的年龄：")
+x=int(x)
+if 0<x<15 :print('玩')
+elif 15<=x<30:print('志于学')
+elif 30<=x<40:print('而立')
+elif 40<=x<50:print('而不惑')
+elif 50<=x<60:print('知天命')
+elif 60<=x<70:print('而耳顺')
+elif x>=70:print('从心所欲')
