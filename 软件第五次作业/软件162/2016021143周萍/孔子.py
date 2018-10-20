f4244edd89000de1df37ef43cbf9d612da8e1535
zhouping1 <43258491+zhouping1@users.noreply.github.com>
2018-10-20 20:24:00 +0800

    2018.9.27

---
+++ 软件第五次作业/软件162/2016021143周萍/孔子.py
@@ -0,0 +1,15 @@
+a=int(input('请输入年龄'))
+if a>=70:
+    print('从心所欲而不逾矩')
+elif a>=60:
+    print('耳顺')
+elif a>=50:
+    print('知天命')
+elif a>=40:
+    print('不惑')
+elif a>=30:
+    print('而立')
+elif a>=15:
+    print('至于学')
+else:
+    print('请重新输入')
