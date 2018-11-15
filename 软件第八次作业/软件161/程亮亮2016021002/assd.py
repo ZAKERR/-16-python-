798186d3ee5afdf3611dd4d527b80bfd6f9b246f
lala666 <31733119+lala666@users.noreply.github.com>
2018-11-15 19:30:40 +0800

    2018年11月15日

--- 软件第八次作业/软件161/程亮亮2016021002/assd.py
+++ 软件第八次作业/软件161/程亮亮2016021002/assd.py
@@ -3,12 +3,6 @@
 #编写时间：20181115
 #编写内容：分词
 
-'''
-s="我想和女朋友一起去北京博物馆" #全模式分词
-cut=jieba.cut(s,cut_all=True)
-print(','.join(cut))
-'''
-
 import jieba
 #jieba.load_userdict("userdict.txt")
 with open("shui.txt",'r',encoding='utf-8') as f:
