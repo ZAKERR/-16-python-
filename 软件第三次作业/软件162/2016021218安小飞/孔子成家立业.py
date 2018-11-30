bc35aec614a8da075a81f35694cb0f874e5e3d19
小飞 <43228179+feixiaoan@users.noreply.github.com>
2018-11-30 10:06:58 +0800

    11

---
+++ 软件第三次作业/软件162/2016021218安小飞/孔子成家立业.py
@@ -0,0 +1,17 @@
+age = int(input('输入孔子年龄：'))
+if age<15:
+    print("玩耍")
+elif age>=15 and age<30:
+    print("学习")
+elif age>=30 and age<40:
+    print("成家立业")
+elif age>=40 and age<50:
+    print("不惑")
+elif age>=50 and age<60:
+    print("知天命")
+elif age>=60 and age<70:
+    print("耳顺")
+else :
+    print("从心所欲，不逾矩")
+
+    
